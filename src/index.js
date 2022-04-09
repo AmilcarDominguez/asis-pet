1f78d7ac1622dc08d2aaed1ac8974fb73da81e83
amilcardominguez <amilcardominguez1234@gmail.com>
2022-04-08 21:45:43 -0400

    Quinto commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const multer = require("multer");
+//const multer = require("multer");
 const app = express();
-const upload = multer({ dest: "uploads/" });
+//const upload = multer({ dest: "uploads/" });
 const cors = require('cors');
-
 app.use(cors());
 app.use(bodyparser());
 
-//Routes del caso de uso 
+//Routes del caso de uso
 require('./routes/usuario.route')(app);
 require('./routes/mascota.route')(app);
-require('./routes/gasto.route')(app);
+require('./routes/gasto-mascota.route')(app);
 app.listen(3000)
